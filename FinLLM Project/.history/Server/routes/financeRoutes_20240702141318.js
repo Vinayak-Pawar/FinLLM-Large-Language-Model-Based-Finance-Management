db07bb003b7b51ff4be41336e9c24e636ad6fe72
Vinayak Pawar <91979292+Vinayak-Pawar@users.noreply.github.com>
2024-09-21 10:15:45 +0100

    Just a save

--- FinLLM Project/.history/Server/routes/financeRoutes_20240702141318.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { getInsights } = require('../controllers/financeController');
-
-const router = express.Router();
-
-router.post('/insights', getInsights);
-
-module.exports = router;
